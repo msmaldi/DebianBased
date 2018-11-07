fa14033789bdd54c4a96ad3bf9733a8c1931cad3
Matheus <msmaldi@hotmail.com>
2018-11-07 09:11:19 -0200

    Elementary Settings

---
+++ scripts/elementary.sh
@@ -0,0 +1,5 @@
+gsettings set io.elementary.terminal.settings cursor-shape Underline
+gsettings set io.elementary.terminal.settings foreground '#FFFFFF'
+gsettings set io.elementary.terminal.settings background '#000000'
+gsettings set io.elementary.terminal.settings palette '#000000:#C00000:#00C000:#C0C000:#0000C0:#C000C0:#00C0C0:#C0C0C0:#3F3F3F:#FF3F3F:#3FFF3F:#FFFF3F:#3F3FFF:#FF3FFF:#3FFFFF:#FFFFFF'
+gsettings set io.elementary.terminal.settings cursor-color '#FFFFFF'
